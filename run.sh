f2ec9aa94a6cbf188a1d60fd0a3a9cdef77e3529
khoirulhasin <irulsits@gmail.com>
2023-09-29 08:55:48 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export DRONE_GITHUB_CLIENT_ID=
-export DRONE_GITHUB_CLIENT_SECRET=
-export DRONE_GITHUB_ADMIN=
-export DRONE_SERVER_HOST=
+export DRONE_GITHUB_CLIENT_ID=cd259f99aafb82a96c00
+export DRONE_GITHUB_CLIENT_SECRET=f6513bbe29ea58fb80830b4faf97c94f1696ce69
+export DRONE_GITHUB_ADMIN=khoirulhasin
+export DRONE_SERVER_HOST=6e16-103-189-201-100.ngrok-free.app
 
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
